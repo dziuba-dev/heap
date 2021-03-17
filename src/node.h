2b85fb13ac54ada9c2822de3233594feb7abd6aa
Emilia <emibyw2000@gmail.com>
2021-03-17 23:31:02 +0100

    Changed variable names in Node class

--- src/node.h
+++ src/node.h
@@ -12,28 +12,30 @@ public:
     Node* left;
     Node* right;
 
-    Node(ValueType v);
-    Node(ValueType v, Node<ValueType>* p);
+    Node(ValueType nodeValue);
+    Node(ValueType nodeValue, Node<ValueType>* nodeParent);
     ~Node() = default;
 
     ValueType getValue();
-    void setValue(ValueType v);
+    void setValue(ValueType newValue);
 
-    friend std::ostream& operator<<(std::ostream& str, Node<ValueType>& n) {
-        return (str << n.value);
+    friend std::ostream& operator<<(std::ostream& stream, Node<ValueType>& node) {
+        return (stream << node.value);
     };
-    friend std::ostream& operator<<(std::ostream& str, Node<ValueType>* n) {
-        return (str << n->value);
+    friend std::ostream& operator<<(std::ostream& stream, Node<ValueType>* node) {
+        return (stream << node->value);
     };
 };
 
 // Constructor with value
 template<class ValueType>
-Node<ValueType>::Node(ValueType v) : value(v), parent(nullptr), left(nullptr), right(nullptr) {}
+Node<ValueType>::Node(ValueType nodeValue) 
+    : value(nodeValue), parent(nullptr), left(nullptr), right(nullptr) {}
 
 // Constructor with value and pointer to parent node
 template<class ValueType>
-Node<ValueType>::Node(ValueType v, Node<ValueType>* p) : value(v), parent(p), left(nullptr), right(nullptr) {
+Node<ValueType>::Node(ValueType nodeValue, Node<ValueType>* nodeParent) 
+    : value(nodeValue), parent(nodeParent), left(nullptr), right(nullptr) {
     if (parent->left == nullptr) {
         parent->left = this;
     }
@@ -53,6 +55,6 @@ ValueType Node<ValueType>::getValue() {
 
 // Change node value
 template<class ValueType>
-void Node<ValueType>::setValue(ValueType v) {
-    value = v;
+void Node<ValueType>::setValue(ValueType newValue) {
+    value = newValue;
 }
